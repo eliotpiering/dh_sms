64de1eaf12bfd34e92aa961ae8113dbf4acc09d7
Eliot Piering <eliot@eliotpiering.com>
2020-07-02 18:09:32 -0500

    wip listing twilio phone numbers

--- lib/dh_sms/messaging.ex
+++ lib/dh_sms/messaging.ex
@@ -118,6 +118,9 @@ defmodule DhSms.Messaging do
     |> Repo.insert()
   end
 
+  def create_and_send_message(attrs \\ %{}) do
+  end
+
   def update_message(%Message{} = message, attrs) do
     message
     |> Message.changeset(attrs)
--- lib/dh_sms/twilio.ex
+++ lib/dh_sms/twilio.ex
@@ -1,8 +1,18 @@
 defmodule DhSms.Twilio do
 
   @twilio_number "+12012855544"
+  @base_url "https://api.twilio.com"
 
   def send_message(to, body) do
     ExTwilio.Message.create(to: to, from: @twilio_number, body: body)
+  end
+
+  def list_phone_numbers() do
+    {:system, name} = Application.get_env(:ex_twilio, :account_sid)
+    account_sid = System.get_env(name)
+
+    {:ok, rsp} = ExTwilio.Api.get("#{@base_url}/2010-04-01/Accounts/#{account_sid}/IncomingPhoneNumbers.json")
+    {:ok, parsed} = rsp.body |> Poison.decode
+    parsed["incoming_phone_numbers"] |> Enum.map (fn p -> p["phone_number"] end)
   end
 end
