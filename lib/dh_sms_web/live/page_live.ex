0074355b18da7e82cae81a735e0e1f893f338489
Eliot Piering <eliot@eliotpiering.com>
2020-07-01 19:26:03 -0500

    Bunch of CSS stuff; displaying current conversations; can create new messages;

--- lib/dh_sms_web/live/page_live.ex
+++ lib/dh_sms_web/live/page_live.ex
@@ -8,25 +8,50 @@ defmodule DhSmsWeb.PageLive do
   def mount(_params, _session, socket) do
     messages = TestMessage.fetch_all()
     all_conversations = Conversation.build_from_messages(messages)
-    {:ok, assign(socket, conversations: all_conversations, current_conversation: nil)}
+    {:ok, assign(socket, conversations: all_conversations, current_conversation: %Conversation{})}
   end
 
   def message_preview(conversation) do
-    message = conversation.messages |> List.first
+    message = conversation.messages |> List.first()
     truncated_body = message.body |> String.slice(0..100)
     "#{truncated_body}..."
   end
 
-  def formatted_last_read_date(conversation) do
-    {:ok, date} = conversation.last_read_datetime |> DateTime.shift_zone("America/Chicago")
+  def formatted_date(date) do
+    {:ok, date} = date |> DateTime.shift_zone("America/Chicago")
     {:ok, formatted} = Calendar.Strftime.strftime(date, "%a %b %e, %l:%M %p")
     formatted
   end
 
-  # @impl true
-  # def handle_event("suggest", %{"q" => query}, socket) do
-  #   {:noreply, assign(socket, results: search(query), query: query)}
-  # end
+  def conversation_message_class(message) do
+    if message.from_dh, do: "column-offset-50 dh-message", else: "user-message"
+  end
+
+  def conversation_mini_class(conversation, current_contact_id) do
+    if (conversation.contact_id == current_contact_id) do
+      "active-conversation"
+    end
+  end
+
+  def messages_reverse_order(conversation) do
+    conversation.messages |> Enum.reverse
+  end
+
+
+  @impl true
+  def handle_event("change_conversation", %{"contact-id" => contact_id}, socket) do
+    conversation = socket.assigns.conversations |> Map.get(contact_id)
+    {:noreply, assign(socket, current_conversation: conversation)}
+  end
+
+
+  @impl true
+  def handle_event("new_message", %{"body" => body, "contact-id" => contact_id}, socket) do
+    #Conversation.add_message(conversation_id, body)
+    conversations = Conversation.add_message_to_conversations(socket.assigns.conversations, %TestMessage{contact_id: contact_id, name: "", body: body, from_dh: true, sent_at: DateTime.utc_now()})
+
+    {:noreply, assign(socket, conversations: conversations, current_conversation: Map.get(conversations, contact_id))}
+  end
 
   # @impl true
   # def handle_event("search", %{"q" => query}, socket) do
